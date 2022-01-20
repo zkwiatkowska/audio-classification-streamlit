fa313965fe8f4d17e2d6db8fb873ee0677d211e3
zkwiatkowska <z.kwiatkowska7@gmail.com>
2022-01-21 00:38:02 +0100

    Added predictions to streamlit app

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import soundfile as sf
 import matplotlib.pyplot as plt
+import torchopenl3
 from librosa import display
 from audio_features import make_spectrogram, make_log_spectrogram, make_logmel_spectrogram, make_mel_spectrogram
+from train import OpenL3Classifier
+import pandas as pd
 
 
 def make_plot(feature, sr: int, name: str, y_axis: str):
@@ -17,11 +20,23 @@ def make_plot(feature, sr: int, name: str, y_axis: str):
     st.pyplot(fig=figs)
 
 
+def make_prediction(audio_file, sampling_rate, net, mapping):
+    feature, _ = torchopenl3.get_audio_embedding(audio_file, sampling_rate, content_type="env", embedding_size=512)
+    feature = feature.mean(axis=1).cpu()
+    prediction = int(net(feature).argmax(axis=1)[0])
+    prediction = mapping.loc[prediction]["category"]
+    prediction = prediction.replace("_", " ").title()
+    return prediction
+
+
 if __name__ == '__main__':
 
     st.header("Upload .wav audio file")
     uploaded_file = st.file_uploader("", type="wav")
     plt.style.use("ggplot")
+    model = OpenL3Classifier(512, 50, 1e-3, 1).load_from_checkpoint("models/fold4/checkpoints/epoch=149-step=599.ckpt")
+    model.eval()
+    class_map = pd.read_csv("class_map.csv")
 
     if uploaded_file is not None:
         audio, sample_rate = sf.read(uploaded_file)
@@ -78,3 +93,6 @@ if __name__ == '__main__':
                 name="**Log-scaled Mel-spectrogram with mel y-axis**",
                 y_axis="mel"
             )
+
+        st.header("Classification")
+        st.write(f"The model predicted your file as: {make_prediction(audio, sample_rate, model, class_map)}")
