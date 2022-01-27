604d331f6b9a3bf51c891a145fc34c969c83f7a5
zkwiatkowska <z.kwiatkowska7@gmail.com>
2022-01-27 22:15:14 +0100

    Fixes in Streamlit app: - classification bar plot - added sidebar - added top k classes in classification

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from librosa import display
 from audio_features import make_spectrogram, make_log_spectrogram, make_logmel_spectrogram, make_mel_spectrogram
 from train import OpenL3Classifier
 import pandas as pd
+import numpy as np
+
+plt.style.use("ggplot")
 
 
 def make_plot(feature, sr: int, name: str, y_axis: str):
@@ -20,79 +23,102 @@ def make_plot(feature, sr: int, name: str, y_axis: str):
     st.pyplot(fig=figs)
 
 
-def make_prediction(audio_file, sampling_rate, net, mapping):
+def make_prediction(audio_file, sampling_rate, net):
     feature, _ = torchopenl3.get_audio_embedding(audio_file, sampling_rate, content_type="env", embedding_size=512)
     feature = feature.mean(axis=1).cpu()
-    prediction = int(net(feature).argmax(axis=1)[0])
-    prediction = mapping.loc[prediction]["category"]
-    prediction = prediction.replace("_", " ").title()
+    return net(feature).detach().numpy()
+
+
+def process_prediction(prediction, mapping, top_k=5):
+    indices = prediction.squeeze().argsort()[-top_k:]
+    classes = list(mapping.loc[indices]["category"])
+    classes = [x.replace("_", " ").title() for x in classes]
+    prediction = list(zip(classes, np.take(prediction, indices)))
     return prediction
+
+
+def setup_environment():
+    loaded_model = OpenL3Classifier(512, 50, 1e-3, 1).load_from_checkpoint(
+        "models/fold4/checkpoints/epoch=149-step=599.ckpt",
+        map_location={"cuda:0": "cpu"}
+    )
+    loaded_model.eval()
+
+    classes = pd.read_csv("class_map.csv")
+    return loaded_model, classes
 
 
 if __name__ == '__main__':
 
+    model, class_map = setup_environment()
+
     st.header("Upload .wav audio file")
     uploaded_file = st.file_uploader("", type="wav")
-    plt.style.use("ggplot")
-    model = OpenL3Classifier(512, 50, 1e-3, 1).load_from_checkpoint("models/fold4/checkpoints/epoch=149-step=599.ckpt")
-    model.eval()
-    class_map = pd.read_csv("class_map.csv")
+
+    window = st.sidebar.selectbox("Choose window size for STFT", options=[128, 1024, 2048, 4096])
+    melbins = st.sidebar.selectbox("Choose number of Mel bins", options=[32, 64, 128])
+    top_k_classes = st.sidebar.slider("Choose number of top K classes", min_value=1, max_value=50, value=5)
 
     if uploaded_file is not None:
-        audio, sample_rate = sf.read(uploaded_file)
-        window_options = [32, 128, 1024, 2048, 4096]
-        window_options = [x for x in window_options if x < len(audio)]
+        if uploaded_file.type != "audio/wav":
+            st.warning("Wrong file type")
+        else:
+            audio, sample_rate = sf.read(uploaded_file)
 
-        st.header("Listen to your audio file")
-        st.audio(uploaded_file)
+            st.header("Listen to your audio file")
+            st.audio(uploaded_file)
 
-        st.header("Waveform")
-        fig = plt.figure(figsize=(10, 5))
-        display.waveplot(audio, sr=sample_rate)
-        st.pyplot(fig=fig)
+            st.header("Waveform")
+            fig = plt.figure(figsize=(10, 5))
+            display.waveplot(audio, sr=sample_rate)
+            st.pyplot(fig=fig)
 
-        st.header("Feature plots")
+            st.header("Feature plots")
+            st.subheader("Short-Term Fourier Transform")
+            col1, col2 = st.columns(2)
 
-        window = st.selectbox("Choose window size for STFT", options=window_options)
-        melbins = st.selectbox("Choose number of Mel bins", options=[32, 64, 128, 256])
+            with col1:
+                make_plot(
+                    feature=make_spectrogram(audio, n_fft=window, hop=window // 4),
+                    sr=sample_rate,
+                    name="**STFT with log y-axis**",
+                    y_axis="log"
+                )
 
-        st.subheader("Short-Term Fourier Transform")
-        col1, col2 = st.columns(2)
+            with col2:
+                make_plot(
+                    feature=make_log_spectrogram(audio, n_fft=window, hop=window // 4),
+                    sr=sample_rate,
+                    name="**Log-scaled STFT with log y-axis**",
+                    y_axis="log"
+                )
 
-        with col1:
-            make_plot(
-                feature=make_spectrogram(audio, n_fft=window, hop=window // 4),
-                sr=sample_rate,
-                name="**STFT with log y-axis**",
-                y_axis="log"
+            st.subheader("Mel Spectrogram")
+            col1, col2 = st.columns(2)
+
+            with col1:
+                make_plot(
+                    feature=make_mel_spectrogram(audio, n_fft=window, hop=window // 4, n_mels=melbins),
+                    sr=sample_rate,
+                    name="**Mel-spectrogram with mel y-axis**",
+                    y_axis="mel"
+                )
+
+            with col2:
+                make_plot(
+                    feature=make_logmel_spectrogram(audio, n_fft=window, hop=window // 4, n_mels=melbins),
+                    sr=sample_rate,
+                    name="**Log-scaled Mel-spectrogram with mel y-axis**",
+                    y_axis="mel"
+                )
+
+            st.header("Classification")
+            answer = make_prediction(audio, sample_rate, model)
+            top_k_predictions = pd.DataFrame(
+                process_prediction(answer, class_map, top_k=top_k_classes),
+                columns=["Class", "Probability"]
             )
 
-        with col2:
-            make_plot(
-                feature=make_log_spectrogram(audio, n_fft=window, hop=window // 4),
-                sr=sample_rate,
-                name="**Log-scaled STFT with log y-axis**",
-                y_axis="log"
-            )
-
-        st.subheader("Mel Spectrogram")
-        col1, col2 = st.columns(2)
-
-        with col1:
-            make_plot(
-                feature=make_mel_spectrogram(audio, n_fft=window, hop=window // 4, n_mels=melbins),
-                sr=sample_rate,
-                name="**Mel-spectrogram with mel y-axis**",
-                y_axis="mel"
-            )
-
-        with col2:
-            make_plot(
-                feature=make_logmel_spectrogram(audio, n_fft=window, hop=window // 4, n_mels=melbins),
-                sr=sample_rate,
-                name="**Log-scaled Mel-spectrogram with mel y-axis**",
-                y_axis="mel"
-            )
-
-        st.header("Classification")
-        st.write(f"The model predicted your file as: {make_prediction(audio, sample_rate, model, class_map)}")
+            fig = plt.figure(figsize=(10, 5))
+            plt.barh(top_k_predictions["Class"], top_k_predictions["Probability"])
+            st.pyplot(fig=fig)
